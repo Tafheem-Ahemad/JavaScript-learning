45e06673475d539f34788770ba4d75a76c2de529
Tafheem <ahemadtafheem@gmail.com>
2024-08-03 23:55:33 +0530

    Objects part 2

--- Objects.js
+++ Objects.js
@@ -29,3 +29,33 @@ me.newfunction2();
 
 const myobject2= new Object(); // It is a Singleton Object
 
+
+
+const obj1={ a : "1" , b:"2"}
+const obj2={ c : "1" , d:"2"}
+
+const combine_obj={...obj1,...obj2}
+
+console.log(combine_obj);
+
+
+const obj4={
+	a: "10",
+	b: '20',
+	c: "30"
+}
+
+console.log(Object.values(obj4));
+console.log(Object.keys(obj4)); // Give a array 
+console.log(Object.entries(obj4));  
+
+
+//  DeStructuring
+const obj5={
+	name : "Tafheem Ahemad",
+	age:20,
+}
+
+const {name : na}=obj5 // Destucturing Part
+console.log(na);
+
